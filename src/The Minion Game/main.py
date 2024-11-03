6528a133645525c7f0560151a21856b685bb2ab9
yousseftarhri <yousseftarhri@gmail.com>
2024-11-03 14:46:57 +0100

    optimisation minion game

--- src/The Minion Game/main.py
+++ src/The Minion Game/main.py
@@ -1,33 +1,15 @@
 def minion_game(string):
     vowels = ["A", "E", "I", "O", "U"]
 
-    kevin_score = 0
-    stuart_score = 0
-    kevin_sublist = []
-    stuart_sublist = []
-
+    kevin_score = stuart_score = 0
+    length = len(string)
     for i in range(len(string)):
         if string[i] in vowels:
-            # Sub string that start with a vowel
-            kevin_sub_string = string[i:]
-            kevin_sublist = kevin_sublist + [kevin_sub_string[0:e + 1] for e in range(len(kevin_sub_string))]
+            kevin_score += length - i
 
         else:
-            # Sub string that start with a vowel
-            stuart_sub_string = string[i:]
-            stuart_sublist = stuart_sublist + [stuart_sub_string[0:a + 1] for a in range(len(stuart_sub_string))]
+            stuart_score += length - i
 
-    for i in list(set(kevin_sublist)):
-        length_i = len(i)
-        for e in range(len(string)):
-            if string[e:e + length_i] == i:
-                kevin_score = kevin_score + 1
-
-    for i in list(set(stuart_sublist)):
-        length_i = len(i)
-        for e in range(len(string)):
-            if string[e:e + length_i] == i:
-                stuart_score = stuart_score + 1
 
     if kevin_score > stuart_score:
         print(f"Kevin {kevin_score}")
