866facc2e97bea7707da9d3836dd5dc914dc99f1
yousseftarhri <yousseftarhri@gmail.com>
2025-01-03 16:34:21 +0100

    merge list o(n)

--- src/data structure/linked_list/merge-two-sorted-linked-lists.py
+++ src/data structure/linked_list/merge-two-sorted-linked-lists.py
@@ -1,5 +1,5 @@
 #https://www.hackerrank.com/challenges/merge-two-sorted-linked-lists/problem?isFullScreen=true
-#!/bin/python3
+# !/bin/python3
 
 import math
 import os
@@ -50,20 +50,28 @@ def print_singly_linked_list(node, sep, fptr):
 #     SinglyLinkedListNode next
 #
 #
+#
 def mergeLists(head1, head2):
-    # base case
-    if head1 is None:
-        return head2
-    if head2 is None:
-        return head1
+    dummy_node = SinglyLinkedListNode(-1)
+    current = dummy_node
 
-    if head1.data <= head2.data:
-        head1.next = mergeLists(head1.next, head2)
-        return head1
+    while head1 is not None and head2 is not None:
 
+        if head1.data <= head2.data:
+            current.next = head1
+            head1 = head1.next
+        else:
+            current.next = head2
+            head2 = head2.next
+
+        current = current.next
+
+    if head1 is not None:
+        current.next = head1
     else:
-        head2.next = mergeLists(head1, head2.next)
-        return head2
+        current.next = head2
+
+    return dummy_node.next
 
 
 if __name__ == '__main__':
